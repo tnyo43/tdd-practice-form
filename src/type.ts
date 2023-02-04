0f1d7c9aeffd12f0b8948c6608b81a19303e5977
Tomoya Kashifuku <tnyo43@gmail.com>
2023-02-04 17:24:23 +0900

    型の定義を簡潔に修正

--- src/type.ts
+++ src/type.ts
@@ -1,11 +1,9 @@
 type KeyofUnion<T> = T extends T ? keyof T : never;
 
 type ComposeRUnion<T, U = T> = T extends T
-  ? { [Key in keyof T]: T[Key] } & Partial<
-      Record<Exclude<KeyofUnion<U>, keyof T>, never>
-    >
+  ? T & Partial<Record<Exclude<KeyofUnion<U>, keyof T>, never>>
   : never;
 
-export type RUnion<T> = ComposeRUnion<T, T>;
+export type RUnion<T> = ComposeRUnion<T>;
 
 export type PartialNull<T> = { [K in keyof T]?: T[K] | null };
